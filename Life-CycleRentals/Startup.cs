7b1666783e0a2777cd98a56e45d3988705ef9ce9
Jack Bradley <jackandrewbradley@gmail.com>
2018-03-26 13:42:06 +0100

    Add project files.

---
+++ Life-CycleRentals/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Life_CycleRentals.Startup))]
+namespace Life_CycleRentals
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
